1d61978bd0419bb6fb2d2c1a15106fa8e0ee2fda
IsmaGold67 <ismaelroberto67@gmail.com>
2024-01-26 23:54:37 -0400

    modify configure var

---
+++ config/firebase-config.js
@@ -0,0 +1,13 @@
+const config = {
+    firebaseConfig : {
+        apiKey: "AIzaSyAbDNHc72qZ4mSOZYOP5Yfe56qF8RAzNtU",
+        authDomain: "testing-firebase-ed8b9.firebaseapp.com",
+         projectId: "testing-firebase-ed8b9",
+         storageBucket: "testing-firebase-ed8b9.appspot.com",
+         messagingSenderId: "204071893005",
+         appId: "1:204071893005:web:ee3ec992f5dc603cb4fd44",
+         measurementId: "G-K4B4KCL6HZ"
+    }
+}
+
+export default config;
